07c2b28c4826fffbbc727822853a6b4820e34679
slothmast3r <oskar.straszynski@gmail.com>
2022-01-03 21:10:34 +0100

    shuffle + input

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <transition-group name="flip-list" tag="div" class="table-wrapper">
+      <tab-element
+          v-for="el in kontener"
+          :key="el.id"
+          :columns-count="columnsCount"
+          :is-active="el.isActive"
+          :title="el.title"
+          @component-clicked="selectElement(el)"
+      />
+    </transition-group>
+    <button @click="kontener = shuffle(kontener);">Shuffle</button>
+    <input v-model="inputElement.title">
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+
+import TabElement from "@/components/TabElement";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TabElement
+  },
+  data(){
+    return {
+      kontener: [],
+      elementsCount: 6,
+      columnsCount: 3,
+      inputElement: {title: ''},
+    }
+  },
+  created() {
+    this.createData()
+  },
+  methods:{
+    createData(){
+      for (let i = 0; i < this.elementsCount; i++) {
+        let obj = {
+          title : 'Tytuł '+ (i+1),
+          id : 'kontener' + i,
+          isActive: false,
+        }
+        this.kontener.push(obj)
+      }
+    },
+    shuffle(array) {
+      let currentIndex = array.length,  randomIndex;
+
+      while (currentIndex != 0) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex], array[currentIndex]];
+      }
+      return array;
+    },
+    selectElement(el){
+      this.inputElement.isActive = false
+      this.inputElement = el
+      this.inputElement.isActive = true
+    },
+    deselectElement(){
+      this.inputElement = false
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +81,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.table-wrapper{
+  display: flex;
+  list-style-type: none;
+  flex-wrap: wrap;
+  margin-bottom: 40px;
+}
+
+
+.flip-list-move {
+  transition: transform 0.8s ease;
+}
+button{
+  margin: 10px;
 }
 </style>
---
+++ src/components/TabElement.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="li-wrapper"
+    :style="{
+      '--columns-count': this.columnsCount,
+      background: isActive ? 'lightgreen' : 'white'
+    }"
+    @click="$emit('component-clicked')"
+  >
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: "TabElement",
+  props:{
+    title:{
+      default: '',
+      type: String,
+    },
+    columnsCount:{
+      default: 3,
+      type: Number,
+    },
+    isActive:{
+      default: false,
+      type: Boolean,
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+li{
+  margin: 0;
+  padding: 0;
+}
+.li-wrapper{
+  width: calc(100% / var(--columns-count) - 52px );
+  padding: 20px;
+  border: 1px solid black;
+  background: white;
+  margin: 5px;
+}
+</style>
